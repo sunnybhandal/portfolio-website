9ff8dd01903c6b7c5d45723b0129f9f6aa33443d
sunnybhandal <sunnybhandal123@gmail.com>
2026-07-16 12:55:16 -0600

    a

--- sections/AboutSection.tsx
+++ sections/AboutSection.tsx
@@ -13,7 +13,7 @@ export default function AboutSection() {
 
         <div className="grid grid-cols-1 md:grid-cols-12 gap-10 lg:gap-14 items-center">
           <div className="md:col-span-5 flex justify-center md:justify-start w-full">
-            <div className="relative w-full aspect-square md:w-80 md:h-80 md:aspect-auto lg:w-96 lg:h-96 rounded-full overflow-hidden shadow-2xl ring-4 ring-cyan-400/70 shadow-cyan-500/20 shrink-0">
+            <div className="relative w-[90%] aspect-square md:w-80 md:h-80 md:aspect-auto lg:w-96 lg:h-96 rounded-full overflow-hidden shadow-2xl ring-4 ring-cyan-400/70 shadow-cyan-500/20 shrink-0">
               <ImageWithFallback
                 src="/deuces.svg"
                 alt="Profile Picture"
